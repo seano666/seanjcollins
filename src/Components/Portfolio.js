c05d0f2a5ef1ef9273f9a97b8957aed3ca077fbe
Sean <mmmburger@gmail.com>
2020-09-15 20:30:51 -0700

    convert to functional components

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,47 +1,44 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Portfolio extends Component {
-  render() {
+const Portfolio = ({ data }) => {
+  if (data) {
+    var projects = data.projects.map(function (projects) {
+      var projectImage = "images/portfolio/" + projects.image;
 
-    if (this.props.data) {
-      var projects = this.props.data.projects.map(function (projects) {
-        var projectImage = 'images/portfolio/' + projects.image;
-        
-        return (
-          <div key={projects.title} className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href={projects.url} title={projects.title}>
-                <img alt={projects.title} src={projectImage} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
-                  </div>
+      return (
+        <div key={projects.title} className="columns portfolio-item">
+          <div className="item-wrap">
+            <a href={projects.url} title={projects.title}>
+              <img alt={projects.title} src={projectImage} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
                 </div>
-              </a>
-            </div>
-          </div>
-        )
-      })
-    }
-
-    return (
-      <section id="portfolio">
-
-        <div className="row">
-
-          <div className="twelve columns collapsed">
-
-            <h1>Check Out Some of My Works.</h1>
-
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-              {projects}
-            </div>
+              </div>
+            </a>
           </div>
         </div>
-      </section>
-    );
+      );
+    });
   }
-}
+
+  return (
+    <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Works.</h1>
+
+          <div
+            id="portfolio-wrapper"
+            className="bgrid-quarters s-bgrid-thirds cf"
+          >
+            {projects}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Portfolio;
